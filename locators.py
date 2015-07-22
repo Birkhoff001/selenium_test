fb09d323e1a76b433a500d617e36ac0bd53354e0
birkhoff <1276267330@qq.com>
2015-07-22 23:58:09 +0800

    000

--- locators.py
+++ locators.py
@@ -1,47 +1,7 @@
-#-*- coding: utf-8 -*-
-import unittest
-#from pageobjects import locators, selenium_server_connection
-#from pageobjects.basepageobject import BasePageObject
-#from pageobjects.basepageelement import BasePageElement
+from selenium.webdriver.common.by import By
 
+class MainPageLocators(object):
+    GO_BUTTON = (By.ID, 'submit')
 
-class UsernameElement(BasePageElement):
-	def __init__(self):
-		self.locators = locators["login.username"]
-
-	def __set__(self, obj, val):
-		se = selenium_server_connection.connection
-		se.type(self.locators, val)
-
-class PasswordElement(BasePageElement):
-	def __init__(self):
-		self.locators = locators["login.password"]
-
-	def __set__(self, obj, val):
-		se = selenium_server_connection.connection
-		se.type(self.locators, val)
-
-class LoginPageObject(BasePageElement):
-	username = UsernameElement()
-	password = PasswordElement()
-
-	def __init__(self, se):
-		self.se = se
-		self.se.open("/login")
-		self.assertEqual("My Application - Login", self.se.get_tile())
-
-	def submit(self):
-		wait_for = "selenium.broserbot.getCurrentWindow().document.getElementByld('LogoutButton)"
-		self.se.click(locators["login.submit"])
-		self.se.wait_for_condition(wait_for, "30000")
-
-
-
-
-
-
-
-
-
-
-
+class SearchResultsPageLocators(object):
+    pass
--- org_search.py
+++ org_search.py
@@ -1,58 +1,23 @@
-#-*- coding: utf-8 -*-
 import unittest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import page
 
-class OrgSearch(unittest.TestCase):
-	def setUp(self):
-		self.dr = webdriver.Firefox()
-		self.dr.get("http://uqee.com")
+class PythonOrgSearch(unittest.TestCase):
 
-#	def test_search_org(self):
-#		main_page = page.MainPage(self.dr)
-#		assert main_page.is_title_matches(), 'incorrect title matches'
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get("http://www.python.org")
 
-	def tearDown(self):
-		self.dr.close()
+    def test_search_in_python_org(self):
+        main_page = page.MainPage(self.driver)
+        assert main_page.is_title_matches(), "python.org title doesn't match."
+        main_page.search_text_element = "pycon"
+        main_page.click_go_button()
+        search_results_page = page.SearchResultsPage(self.driver)
+        assert search_results_page.is_results_found(), "No results found."
 
-
-	#def testDefaultSize(self):
-	#	assert self.widget.size() == (50, 50), 'incorrect default size'
-
-	#def testResize(self):
-	#	self.widget.resize(100, 150)
-#		assert self.widget.size() == (100, 150), 'wrong size after resize'
-
-#test suite
-#	widgetTestSuite = unittest.TestSuite()
-#	widgetTestSuite.addTest(WidgetTestCase("testDefaultSize"))
-#	widgetTestSuite.addTest(WidgetTestCase("testResize"))
-
-class WidgetTestSuite(unittest.TestSuite):
-	def __init__(self):
-		unittest.TestSuite.__init__(self,map(WidgetTestCase, ("testDefaultSize", "testResize")))
-		suite = unittest.makeSuite(WidgetTestSuite, 'test')    #makesuite, and use the function sequence that test function name for init func of cmp.
-
-
-	def suite():
-		suite = unittest.TestSuite()
-		suite.addTest(WidgetTestCase("testDefaultSize"))
-		suite.addTest(WidgetTestCase("testResize"))
-		return suite
-		#testsuite group
-		suite1 = module1.TheTestSuite()
-		suite2 = module2.TheTestSuite()
-		alltests = unittest.TestSuite((suite1, suite2))
-		#run test and test report
-		runner = unittest.TextTestRunner()
-		runner.run(widgetTestSuite)
-
-
-
-
+    def tearDown(self):
+        self.driver.close()
 
 if __name__ == "__main__":
-	unittest.main()
-
-
-
+    unittest.main()
