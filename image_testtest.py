30ca0d2144f6a1dbe6c3ee8d7f691f11050aa85b
birkhoff <1276267330@qq.com>
2015-09-07 18:14:03 +0800

    edit file.

--- image_testtest.py
+++ image_testtest.py
@@ -7,47 +7,43 @@ import ImageFilter
 Duration = 1000
 #open the imgge
 def open_pic():
-	try:
-		img = Image.open(img)
-		print img.format, img.size, img.mode
-	except Exception, e:
-		print "load imgge ......failed!"
-	img.show()
+    try:
+        img = Image.open(img)
+        print img.format, img.size, img.mode
+    except Exception, e:
+        print "load imgge ......failed!"
+    img.show()
 #binaryzation
 def binaryzation():
-	try:
-		#coordinate = (100, 100, 200,200)
-		#region = img.crop(coordinate)
+    try:
+        #coordinate = (100, 100, 200,200)
+        #region = img.crop(coordinate)
 		#region.show()
 		#region = region.transpose(Image.ROTATE_180)
         #region.convert("RGBA")
+        imgfilter = img.filter(ImageFilter.DETAIL)
+        imgfilter.show()
+        for y in xrange(img.size[1]):
+            for x in xrange(img.size[0]):
+                if img.load()[x,y][0] < 90:
+                    img.load()[x,y] = (0,0,0,255)
+        for y in xrange(img.size[1]):
+            for x in xrange(img.size[0]):
+                if img.load()[x,y][1] < 136:
+                    img.load()[x,y] = (0,0,0,255)
+        for y in xrange(img.size[1]):
+            for x in xrange(img.size[0]):
+                if img.load[x,y][2] > 0:
+                    img.load[x,y] = (255,255,255,255)
+    except Exception, e:
+        print "the binaryzation failed..."
+    img.show()
+    img.save(".\\1113_black.gif", "GIF")
+    #blow up imgge
+    img_black = Image.open('.\\1113_black.gif').resize((1000, 500), Image.NEAREST)
+    return img_black
 
-		imgfilter = img.filter(ImageFilter.DETAIL)
-		imgfilter.show()
-		for y in xrange(img.size[1]):
-			for x in xrange(img.size[0]):
-				if img.load()[x,y][0] < 90:
-					img.load()[x,y] = (0,0,0,255)
-
-		for y in xrange(img.size[1]):
-			for x in xrange(img.size[0]):
-				if img.load()[x,y][1] < 136:
-					img.load()[x,y] = (0,0,0,255)
-
-		for y in xrange(img.size[1]):
-			for x in xrange(img.size[0]):
-				if img.load[x,y][2] > 0:
-					img.load[x,y] = (255,255,255,255)
-
-	except Exception, e:
-		print "the binaryzation failed..."
-	img.show()
-	img.save(".\\1113_black.gif", "GIF")
-	#blow up imgge
-	img_black = Image.open('.\\1113_black.gif').resize((1000, 500), Image.NEAREST)
-	return img_black
-
-	print "imgge binaryzation success!"
+    print "imgge binaryzation success!"
 
 def division():
     try:
@@ -79,9 +75,12 @@ def recognize(img):
                     points.append((diffs, mod[0]))
                 points.sort()
                 result += points[0][1]
-    return result
+                return result
+    except Exception, e:
+        print "recognize failed..." + str(e)
+
 
 if __name__ == "__main__":
-    imgge_test()
-    
+
+
 
